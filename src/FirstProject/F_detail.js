ea84b82f8e235912a9dce3d04afd258bb483879c
vishesh425 <visheshparmar99v4@gmail.com>
2024-06-20 23:14:45 +0530

    update Project details

--- src/FirstProject/F_detail.js
+++ src/FirstProject/F_detail.js
@@ -1,4 +1,6 @@
 import mockup from "../assets/jpeg/project-mockup-example.jpeg"
+import Project_1 from "../assets/png/project_1.png"
+
 function F_detail(){
     return(
         <section class="project-details">
@@ -6,7 +8,7 @@ function F_detail(){
           <div class="project-details__content">
             <div class="project-details__showcase-img-cont">
               <img
-                src={mockup}
+                src={Project_1}
                 alt="Project Image"
                 class="project-details__showcase-img"
               />
@@ -15,21 +17,17 @@ function F_detail(){
               <div class="project-details__desc">
                 <h3 class="project-details__content-title">Project Overview</h3>
                 <p class="project-details__desc-para">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
-                  alias tenetur minus quaerat aliquid, aut provident blanditiis,
-                  deleniti aspernatur ipsam eaque veniam voluptatem corporis vitae
-                  mollitia laborum corrupti ullam rem. Lorem ipsum dolor sit amet
-                  consectetur adipisicing elit. Neque alias tenetur minus quaerat
-                  aliquid, aut provident blanditiis, deleniti aspernatur ipsam
-                  eaque veniam voluptatem corporis vitae mollitia laborum corrupti
-                  ullam rem?
+                Cial is a dynamic web application that combines the power of HTML, CSS, and JavaScript
+                 to deliver a user-friendly and interactive financial services platform. 
+                 This project serves as a showcase of modern web development techniques and
+                  includes robust version control using Git for seamless collaboration and code management.
                 </p>
-                <p class="project-details__desc-para">
+                {/* <p class="project-details__desc-para">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
                   alias tenetur minus quaerat aliquid, aut provident blanditiis,
                   deleniti aspernatur ipsam eaque veniam voluptatem corporis vitae
                   mollitia laborum corrupti ullam rem?
-                </p>
+                </p> */}
               </div>
               <div class="project-details__tools-used">
                 <h3 class="project-details__content-title">Tools Used</h3>
@@ -37,27 +35,27 @@ function F_detail(){
                   <div class="skills__skill">HTML</div>
                   <div class="skills__skill">CSS</div>
                   <div class="skills__skill">JavaScript</div>
-                  <div class="skills__skill">React</div>
-                  <div class="skills__skill">SASS</div>
+                  {/* <div class="skills__skill">React</div>
+                  <div class="skills__skill">SASS</div> */}
                   <div class="skills__skill">GIT</div>
-                  <div class="skills__skill">Shopify</div>
-                  <div class="skills__skill">Wordpress</div>
-                  <div class="skills__skill">Google ADS</div>
-                  <div class="skills__skill">Facebook Ads</div>
-                  <div class="skills__skill">Android</div>
-                  <div class="skills__skill">IOS</div>
+                  {/* <div class="skills__skill">Shopify</div>
+                  <div class="skills__skill">Wordpress</div> */}
+                  <div class="skills__skill">Google Map</div>
+                  {/* <div class="skills__skill">Facebook Ads</div> */}
+                  {/* <div class="skills__skill">Android</div>
+                  <div class="skills__skill">IOS</div> */}
                 </div>
               </div>
               <div class="project-details__links">
                 <h3 class="project-details__content-title">See Live</h3>
                 <a
-                  href="#"
+                  href="https://vishesh425.github.io/Cial/"
                   class="btn btn--med btn--theme project-details__links-btn dynamicBgClr"
                   target="_blank"
                   >Live Link</a
                 >
                 <a
-                  href="#"
+                  href="https://github.com/vishesh425/Cial"
                   class="btn btn--med btn--theme-inv project-details__links-btn dynamicBrdrClr"
                   target="_blank"
                   >Code Link</a
